83550d0d4943568b67a77173baa080b5442310df
lordmarcusvane <mariojacobo2011@gmail.com>
2021-09-15 15:04:22 -0500

    avance

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {Text, View, SafeAreaView, StyleSheet, Image} from 'react-native';
-import { colors } from './utils/colors';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from 'react-native';
+import {colors} from './utils/colors';
 
 const containerStyles = StyleSheet.create({
   containerTopBar: {
     flex: 0.08,
-    justifyContent: 'center',
+    flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: colors.primary,
   },
@@ -31,19 +28,40 @@ const containerStyles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'yellow',
   },
+  generalContainer: {
+    flex: 1,
+  },
+});
+
+const topBarStyles = StyleSheet.create({
+  logo: {
+    flex: 0.35,
+    marginLeft: 8,
+  },
+  divider: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
+    flex: 10
+  },
 });
 
 const App = () => {
   return (
-    <>
+    <View style={containerStyles.generalContainer}>
       <View style={containerStyles.containerTopBar}>
-        <Image source={require('./assets/images/escudo.png')}/>
+        <Image
+          source={require('./assets/images/escudo.png')}
+          style={topBarStyles.logo}
+          resizeMode="contain"
+        />
+        <View style={topBarStyles.divider}/>
       </View>
       <View style={containerStyles.containerTitle}>
-        <Text>MAMAMA</Text>
+        <Text>jjj</Text>
       </View>
       <View style={containerStyles.left}></View>
-    </>
+    </View>
   );
 };
 
