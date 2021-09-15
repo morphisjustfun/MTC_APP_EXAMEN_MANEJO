668cd6716c45b069997f900792c101fefaf5a2d9
lordmarcusvane <mariojacobo2011@gmail.com>
2021-09-14 20:42:44 -0500

    init

---
+++ android/app/src/main/java/com/mtc_app_examen_maneo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mtc_app_examen_maneo;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "MTC_APP_EXAMEN_MANEO";
+  }
+}
