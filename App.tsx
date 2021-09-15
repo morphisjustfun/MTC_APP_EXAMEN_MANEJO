dcc429fd5a2e548c9975a6c754860a8ca074f2f5
lordmarcusvane <mariojacobo2011@gmail.com>
2021-09-14 21:43:45 -0500

    avance Primera Clase

--- App.tsx
+++ App.tsx
@@ -9,13 +9,41 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, SafeAreaView, StyleSheet, Image} from 'react-native';
+import { colors } from './utils/colors';
+
+const containerStyles = StyleSheet.create({
+  containerTopBar: {
+    flex: 0.08,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.primary,
+  },
+  containerTitle: {
+    flex: 0.46,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+  },
+  left: {
+    flex: 0.46,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+  },
+});
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola</Text>
-    </View>
+    <>
+      <View style={containerStyles.containerTopBar}>
+        <Image source={require('./assets/images/escudo.png')}/>
+      </View>
+      <View style={containerStyles.containerTitle}>
+        <Text>MAMAMA</Text>
+      </View>
+      <View style={containerStyles.left}></View>
+    </>
   );
 };
 
---
+++ utils/colors.ts
@@ -0,0 +1,3 @@
+export const colors = {
+   primary: '#BF0909'
+}
