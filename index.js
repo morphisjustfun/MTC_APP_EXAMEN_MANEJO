0e3dd9953436f7d29930e232978b05a939774b53
lordmarcusvane <mariojacobo2011@gmail.com>
2021-09-19 01:16:32 -0500

    reactNavigation Implemented

--- index.js
+++ index.js
@@ -2,8 +2,28 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('com.MTC.manejo',() => App)
+Navigation.events().registerAppLaunchedListener(() => {
+   Navigation.setRoot({
+      root:{
+         stack:{
+            children:[
+               {
+                  component: {
+                     name: 'com.MTC.manejo'
+                  }
+               }
+            ]
+         }
+      }
+   })
+})
+
+Navigation.setDefaultOptions({
+   topBar:{
+      visible: false
+   }
+})
